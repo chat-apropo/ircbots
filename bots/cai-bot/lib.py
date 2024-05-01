1987886cb13ee81c8e31ca089e812db3ac139da7
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-05-01 22:56:31 +0200

    Work in progress bot for the CAI project

---
+++ bots/cai-bot/lib.py
@@ -0,0 +1,122 @@
+import asyncio
+import logging
+import os
+import poplib
+from contextlib import asynccontextmanager
+from email.parser import Parser
+from typing import AsyncIterator
+from urllib.parse import quote
+
+from async_lru import alru_cache
+from bs4 import BeautifulSoup
+from characterai import aiocai, authUser, sendCode
+from characterai.aiocai.client import WSConnect
+from characterai.types.chat2 import BotAnswer, ChatData
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POP3_SERVER = os.getenv("POP3_SERVER")
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+PASSWORD = os.getenv("PASSWORD")
+assert POP3_SERVER, "POP3_SERVER is required"
+assert EMAIL_ADDRESS, "EMAIL_ADDRESS is required"
+assert PASSWORD, "PASSWORD is required"
+
+
+def get_token_from_email():
+    pop_conn = poplib.POP3_SSL(POP3_SERVER)
+    pop_conn.user(EMAIL_ADDRESS)
+    pop_conn.pass_(PASSWORD)
+
+    # Get the latest email
+    num_messages = len(pop_conn.list()[1])
+    response, raw_messages, octets = pop_conn.retr(num_messages)
+    raw_message = b"\n".join(raw_messages)
+
+    # Parse the email
+    email_parser = Parser()
+    msg = email_parser.parsestr(raw_message.decode("utf-8"))
+    # subject = msg["subject"]
+
+    url = None
+    # Iterate over the parts of the email
+    for part in msg.walk():
+        if part.get_content_type() == "text/html":
+            html_content = part.get_payload(decode=True).decode(part.get_content_charset())
+            soup = BeautifulSoup(html_content, "html.parser")
+            links = soup.find_all("a")
+
+            for link in links:
+                url = link.get("href")
+
+    pop_conn.quit()
+    return url
+
+
+@alru_cache
+async def get_client() -> aiocai.Client:
+    sendCode(EMAIL_ADDRESS)
+    await asyncio.sleep(5)
+    url = get_token_from_email()
+    if not url:
+        raise ValueError("Could not get token from email")
+
+    token = authUser(url, EMAIL_ADDRESS)
+    logging.info(f"Got token: {token}")
+    return aiocai.Client(token=token)
+
+
+async def refresh_client() -> aiocai.Client:
+    # drop cache for get_client
+    get_client.cache_clear()
+    return await get_client()
+
+
+@asynccontextmanager
+async def new_chat(char_id: str) -> AsyncIterator[tuple[ChatData, BotAnswer, WSConnect]]:
+    client = await get_client()
+    me = await client.get_me()
+    async with await client.connect() as conn:
+        new, answer = await conn.new_chat(char_id, str(me.id))
+        yield new, answer, conn
+
+
+@asynccontextmanager
+async def open_chat(char_id: str) -> AsyncIterator[tuple[ChatData, WSConnect]]:
+    client = await get_client()
+    async with await client.connect() as conn:
+        new = await conn.get_chat(char_id)
+        yield new, conn
+
+
+async def main():
+    """REPL for testing"""
+    client = await get_client()
+    me = await client.get_me()
+    print(me)
+
+    c = await client.search(quote("jesus"))
+    char = c[0]
+    print(f"{char.title} ({char.greeting}) - {char.external_id}")
+
+    async with new_chat(char.external_id) as (new, answer, conn):
+        print(f"{answer.name}: {answer.text}")
+        for i in range(3):
+            text = input("YOU: ")
+            message = await conn.send_message(char.external_id, new.chat_id, text)
+            print(f"{message.name}: {message.text}")
+
+    c = await client.search(quote("elon"))
+    char = c[0]
+    print(f"{char.title} ({char.greeting}) - {char.external_id}")
+    async with new_chat(char.external_id) as (new, answer, conn):
+        print(f"{answer.name}: {answer.text}")
+        while True:
+            text = input("YOU: ")
+            message = await conn.send_message(char.external_id, new.chat_id, text)
+            print(f"{message.name}: {message.text}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- bots/cai-bot/main.py
+++ bots/cai-bot/main.py
@@ -1,76 +1,91 @@
 import asyncio
+import collections
 import json
 import os
 import re
 
-import trio_asyncio
+import bottom
 from dotenv import load_dotenv
-from IrcBot.bot import IrcBot, Message, utils
-from PyCAI2 import PyAsyncCAI2
+
+from lib import get_client, new_chat
 
 load_dotenv()
 
 HOST = os.getenv("IRC_HOST")
 assert HOST, "IRC_HOST is required"
-
 PORT = int(os.getenv("IRC_PORT") or 6667)
 NICK = os.getenv("NICK") or "caibot"
 PASSWORD = os.getenv("PASSWORD") or None
 CHANNELS = json.loads(os.getenv("CHANNELS") or "[]")
-OWNER_ID = os.getenv("OWNER_ID")
-AUTHOR = os.getenv("AUTHOR")
-assert AUTHOR, "AUTHOR is required"
-
-AUTHOR_ID = os.getenv("AUTHOR_ID")
 CHAR = os.getenv("CHAR")
 CHAT_ID = os.getenv("CHAT_ID")
 
-aut_set = {
-    "author_id": AUTHOR_ID,  # CREATOR ID
-    "is_human": True,  # PLEASE DON'T WRITE TO FALSE
-    "name": "YOU:",  # YOUR CAI NAME
-}
+assert CHAR, "CHAR is required"
+assert CHAT_ID, "CHAT_ID is required"
 
 
-client = PyAsyncCAI2(OWNER_ID)  # IMPORT OWNER ID
+class Python310Client(bottom.Client):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        *,
+        encoding: str = "utf-8",
+        ssl: bool = True,
+        loop: asyncio.AbstractEventLoop | None = None
+    ) -> None:
+        """Fix 3.10 error"""
+        super().__init__(host, port, encoding=encoding, ssl=ssl, loop=loop)
+        self._events = collections.defaultdict(lambda: asyncio.Event())
 
 
-@utils.regex_cmd_with_messsage(rf"(?i)^((?:.*\s)?{NICK}([\s|,|\.|\;|\?|!|:]*)(?:\s.*)?)$", False)
-async def mention(bot: IrcBot, args: re.Match, message: Message):
-    text = args[1].strip()
-    last = args[2] if args[2] else ""
-    text.replace(f" {NICK}{last}", " ")
-    nick = message.sender_nick
-    answer = await trio_asyncio.aio_as_trio(reply)(text)
-    return f"{nick}: {answer}"
+bot = bottom.Client(HOST, PORT, ssl=False)
 
 
-async def reply(text: str) -> str:
-    async with client.connect(OWNER_ID) as chat2:  # Make a connection to the server
-        r = await chat2.send_message(CHAR, text, AUTHOR, Return_name=False)  # ALL VARIABLES WILL BE SENT TO SERVER
-    return r
+# @bot.regex_cmd_with_messsage(rf"(?i)^((?:.*\s)?{NICK}([\s|,|\.|\;|\?|!|:]*)(?:\s.*)?)$", False)
+# async def mention(bot: IrcBot, args: re.Match, message: Message):
+#     text = args[1].strip()
+#     nick = message.sender_nick
+#     answer = await trio_asyncio.aio_as_trio(reply)(text)
+#     return f"{nick}: {answer}"
 
 
-async def onConnect(bot: IrcBot):
+# async def reply(text: str) -> str:
+#     async with new_chat(CHAT_ID) as (new, answer, conn):
+#         anwer = await conn.send_message(CHAR, new.chat_id, text)
+#     return answer.text
+
+
+@bot.on("PING")
+def keepalive(message, **kwargs):
+    bot.send("PONG", message=message)
+
+
+@bot.on("CLIENT_CONNECT")
+async def connet(**kwargs):
+    await get_client()
+
+    bot.send("NICK", nick=NICK)
+    bot.send("USER", user=NICK, realname=NICK)
+
+    # Don't try to join channels until the server has
+    # sent the MOTD, or signaled that there's no MOTD.
+    done, pending = await asyncio.wait(
+        [bot.wait("RPL_ENDOFMOTD"), bot.wait("ERR_NOMOTD")], return_when=asyncio.FIRST_COMPLETED
+    )
+
+    # Cancel whichever waiter's event didn't come in.
+    for future in pending:
+        future.cancel()
+
     for channel in CHANNELS:
-        await bot.join(channel)
-    await bot.send_message("Hello everyone !!!")
+        bot.send("JOIN", channel=channel)
 
-
-async def repl_loop():
-    message = input("You:")  # INPUT TEXT
-    async with client.connect(OWNER_ID) as chat2:  # Make a connection to the server
-        r = await chat2.send_message(CHAR, message, AUTHOR, Return_name=True)  # ALL VARIABLES WILL BE SENT TO SERVER
-        print(r)
-
-
-# Useful in case we need testing locally
-def repl():
-    while True:
-        asyncio.run(repl_loop())
+    async with new_chat(CHAT_ID) as (new, answer, conn):
+        for channel in CHANNELS:
+            bot.send("PRIVMSG", target=channel, message=answer.text)
 
 
 if __name__ == "__main__":
-    # repl()
-    bot = IrcBot(HOST, PORT, NICK, CHANNELS, PASSWORD)
-    bot.runWithCallback(onConnect)
+    bot.loop.create_task(bot.connect())
+    bot.loop.run_forever()
